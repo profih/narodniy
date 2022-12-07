693d40ff0a868331d62d4c205ede98c02a5857e1
igor.kiria <yisgit@gmail.com>
2022-12-07 11:10:54 +0400

    Блок с видео

---
+++ local/templates/nd/components/dj/dj.landing.section/nr-videos/template.php
@@ -0,0 +1,76 @@
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var CBitrixComponent $this */
+/** @var array $arParams */
+/** @var array $arResult */
+/** @var string $componentPath */
+/** @var string $componentName */
+/** @var string $componentTemplate */
+/** @global CDatabase $DB */
+/** @global CUser $USER */
+/** @global CMain $APPLICATION */
+?>
+
+<section class="videos" id="videos">
+    <div class="videos__container centered-container">
+        <span class="videos__main-text">
+            Один из самых популярных роликов про вторую перегонку,
+            выполненную на аппарате Народный New
+        </span>
+        <div class="videos__main-wrapper">
+            <div class="videos-block">
+                <div class="videosWrapper">
+                    <!-- Copy & Pasted from YouTube -->
+                    <iframe width="560" height="349" src="https://www.youtube.com/embed/NMTsvNplKyQ?rel=0&hd=1"
+                            frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+        <span class="videos__additional-text">
+            Видеообзоры на Народный New
+        </span>
+        <div class="videos__additional-wrapper">
+            <div class="videos-block">
+                <div class="videosWrapper">
+                    <!-- Copy & Pasted from YouTube -->
+                    <iframe width="560" height="349" src="https://www.youtube.com/embed/bAVhe8zE3Y0?rel=0&hd=1"
+                            frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="videos-block">
+                <div class="videosWrapper">
+                    <!-- Copy & Pasted from YouTube -->
+                    <iframe width="560" height="349" src="https://www.youtube.com/embed/C6Y0cWh6kEI?rel=0&hd=1"
+                            frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="videos-block">
+                <div class="videosWrapper">
+                    <!-- Copy & Pasted from YouTube -->
+                    <iframe width="560" height="349" src="https://www.youtube.com/embed/aLTzkGhI-DQ?rel=0&hd=1"
+                            frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="videos-block">
+                <div class="videosWrapper">
+                    <!-- Copy & Pasted from YouTube -->
+                    <iframe width="560" height="349" src="https://www.youtube.com/embed/lqJukj5duks?rel=0&hd=1"
+                            frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="videos-block">
+                <div class="videosWrapper">
+                    <!-- Copy & Pasted from YouTube -->
+                    <iframe width="560" height="349" src="https://www.youtube.com/embed/Tjr51k14Cm4?rel=0&hd=1"
+                            frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="videos-block">
+                <div class="videosWrapper">
+                    <!-- Copy & Pasted from YouTube -->
+                    <iframe width="560" height="349" src="https://www.youtube.com/embed/UFyMh3YiMKE?rel=0&hd=1"
+                            frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
